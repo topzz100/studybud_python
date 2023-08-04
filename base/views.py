57f719f616172dbd350a700657965b7682d9902f
Temitope Adenekan <adenekantope@gmail.com>
2023-08-04 03:58:32 +0100

    feat:

--- base/views.py
+++ base/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from .models import Room
+from django.shortcuts import render, redirect
+from .models import Room, Topic
+from .forms import RoomForm
+# from django.http import HttpResponse
+
 # from django.http import HttpResponse
 
 
@@ -11,8 +14,13 @@ from .models import Room
 
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    rooms = Room.objects.filter(topic__name__icontains=q)
+
+    topics = Topic.objects.all()
+
+    context = {'rooms': rooms, 'topics': topics}
     # return HttpResponse('Home page')
     return render(request, 'base/home.html', context)
 
@@ -24,4 +32,48 @@ def room(request, pk):
     # return HttpResponse('ROOM')
     return render(request, 'base/room.html', context)
 
-# Create your views here.
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html',  context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    # print(str(pk))
+    if request.method == 'POST':
+        # print(request.POST)
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html')
+
+# def deleteRoom(request, pk):
+#     print("pk:", pk)
+#     try:
+#         room = Room.objects.get(id=pk)
+#         print("room:", room)
+#     except Room.DoesNotExist:
+#         print("Room does not exist.")
+#         # return HttpResponse("Room not found.", status=404)
+#     return render(request, 'base/delete.html')
+    # Rest of the view function...
