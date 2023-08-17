76b80662a625a3fafd1f23022bbbc8701f28b210
Temitope Adenekan <adenekantope@gmail.com>
2023-08-17 10:52:56 +0100

    feat: Login and Register

--- base/forms.py
+++ base/forms.py
@@ -1,9 +1,17 @@
 from django.forms import ModelForm
 from .models import Room
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 
 class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = '__all__'
-        exclude = ['host', 'participants', 'about']
+        exclude = ['host', 'participants']
+
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
